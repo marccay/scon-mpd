ae6a0f25e67b7b023b97f219c2d455e799fb3f8a
marccay <info@marccayanong.com>
2018-06-08 03:59:25 -0400

    added nil strings to blank song info data, was initially left blanked

--- current.go
+++ current.go
@@ -7,7 +7,16 @@ import (
 func currentSong() {
 	lines := getMap("currentsong")
 	artist := lines["Artist"]
+	if artist == "" {
+		artist = "nil"
+	}
 	album := lines["Album"]
+	if album == "" {
+		album = "nil"
+	}
 	title := lines["Title"]
+	if title == "" {
+		title = "nil"
+	}
 	fmt.Println(artist + " : " + album + " - " + title)
 }
